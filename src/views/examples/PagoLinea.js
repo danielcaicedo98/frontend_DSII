78c0259724d9a99ab3b435a45ee558d595c10093
nandolopez7 <hernando.rincon@correounivalle.edu.co>
2023-04-11 00:50:34 -0500

    Ajuste de tipografia, cambio de diseño

--- src/views/examples/PagoLinea.js
+++ src/views/examples/PagoLinea.js
@@ -5,26 +5,36 @@ const PagoLinea = () => {
 
     return (
         <>
-            <div className="header p-4 bg-light text-dark rounded" alt="...">
-                <h1 class="display-3 text-dark">
-                    Pago en línea (Paypal)
+            <div className="header p-4 bg-white text-dark rounded" alt="..." >
+                
+                <h1 class="display-2" align="center">
+                    Bienvenido al Portal de pago en línea (Paypal)
                 </h1>
-                <p>
+                <h2>
                     En esta sección podrás realizar el pago de tu factura en linea a través de paypal, para ello vas a necesitar a la mano: <br />
-                </p>
-                <dl>
-                    <li>Cuenta Paypal</li>
-                    <li>Contraseña</li>
-                    <li>Fondos suficientes</li>
-                </dl>
- 
-                <p>
-                    Para proceder con el pago, de click al siguiente botón:
-                </p>
+                </h2>
 
-                <div>
+                <h3>
+                    <dl>
+                        <em>
+                            <li>Cuenta Paypal</li>
+                            <li>Contraseña</li>
+                            <li>Fondos suficientes</li>
+                        </em>
+                    </dl>
+                </h3>
+ 
+                <h2>
+                    Para proceder con el pago, de click al siguiente botón:
+                </h2>
+
+                <div className="mt-4" align="center">
                     <a className="paypal-button" type="button"><img className="h-25" width='25' src={require("../../assets/img/brand/paypal.png")}></img>Paypal</a>
                 </div>
+
+                <footer align="center">    
+                    <img className="h-25" width='40%' src={require("../../assets/img/brand/paypal-credit-card-logo.png")}></img>
+                </footer>
 
             </div>
 
