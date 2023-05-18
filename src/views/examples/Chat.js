885b547198712cddfec11e3e625f5f3796405879
DiazSSJ <juans220304@gmail.com>
2023-05-18 14:28:14 -0500

    estilos para el chat del usuario

--- src/views/examples/Chat.js
+++ src/views/examples/Chat.js
@@ -3,62 +3,74 @@ import { io } from 'socket.io-client';
 const socket = io('http://localhost:3000');
 const Chat = () => {
 
-    const[isConnected, setIsconnected] = useState();
+    const [isConnected, setIsconnected] = useState();
     const [nuevoMensaje, setNuevoMensaje] = useState('');
     const [mensajes, setMensajes] = useState([]);
- 
+
     useEffect(() => {
-        
-        if(socket.on().connected)
+
+        if (socket.on().connected)
             setIsconnected(true)
         socket.on('chat_message', (data) => {
             setMensajes(mensajes => [...mensajes, data]);
             console.log(data)
         });
-        
+
         return () => {
             socket.off('connect');
             socket.off('chat_message');
         }
-        
-    },[]);
+
+    }, []);
 
     const enviarMensaje = () => {
 
         socket.emit('chat_message', {
-          usuario: socket.id,
-          mensaje: nuevoMensaje
+            usuario: socket.id,
+            mensaje: nuevoMensaje
         });
         setNuevoMensaje('');
-      }
-    return(
-        <div className="header bg-gradient-info  ">
+    }
+    return (
+        <div className="header bg-gradient-info rounded">
+            <div className="text-center">
+                <h1>Bienvenido a NRG-X</h1>
+            </div>
+
             {/* <div>{isConnected ? 'CONECTADO': 'NO CONECTADO'}</div> */}
-            <div class="mb-3 m-4 p-3 bg-light ">                
-                <div class="mb-3">
-                <label for="exampleFormControlTextarea1 p-7" class="form-label">Mensajes</label>
-                <div className='bg-white p-2 border rounded' >
-                    <ul>
-                        {mensajes.map ((mensaje) => (
-                                mensaje.usuario === 'ADMIN' ?
-                                <li>{mensaje.usuario} : {mensaje.mensaje}</li>:
-                                <li>TU: {mensaje.mensaje}</li>
-                                
-                            ))
-                        }
-                    </ul>
+            <div className="mb-3 mx-4 p-3 bg-light border rounded mt-3">
+                <div className="mb-3">
+                    <label htmlFor="exampleFormControlTextarea1" className="form-label">
+                        Mensajes
+                    </label>
+                    <div className="bg-white p-2 border rounded" style={{ height: '300px', width: '400px' }}>
+                        <ul style={{ maxHeight: '100%', overflowY: 'scroll' }}>
+                            {mensajes.map((mensaje) => (
+                                <li>
+                                    {mensaje.usuario === 'ADMIN' ? (
+                                        <>
+                                            {mensaje.usuario}: {mensaje.mensaje}
+                                        </>
+                                    ) : (
+                                        <>TU: {mensaje.mensaje}</>
+                                    )}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <input
+                        type="text"
+                        className="form-control mt-3"
+                        id="Escribe tu mensaje"
+                        placeholder="Escribe tu mensaje"
+                        value={nuevoMensaje}
+                        onChange={(e) => setNuevoMensaje(e.target.value)}
+                    />
                 </div>
-                <input 
-                    type="text" 
-                    className="form-control " 
-                    id="Escribe tu mensajes" 
-                    placeholder="Escribe tu mensaje"
-                    value={nuevoMensaje}
-                    onChange={e => setNuevoMensaje(e.target.value)}
-                   ></input>            
-                </div>
-                <button className='btn bg-green'  onClick={enviarMensaje}>Enviar</button> 
-            </div>            
+                <button className="btn btn-primary" onClick={enviarMensaje}>
+                    Enviar
+                </button>
+            </div>
         </div>
 
 
