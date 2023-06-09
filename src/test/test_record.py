566f7e299b103d06ea1b46f746e28f354e7c032a
danielcaicedo98 <daniel.andres.caicedo@correounivalle.edu.co>
2023-06-08 21:41:36 -0500

    Más casos de Pruebas Funcionales

--- src/test/test_record.py
+++ src/test/test_record.py
@@ -38,6 +38,7 @@ WebDriverWait(driver, 10).until(
 
 # Verifica si el inicio de sesión fue exitoso
 print(driver.current_url == 'http://localhost:3000/admin/homeadmin')
+
 if 'http://localhost:3000/admin/homeadmin' in driver.current_url:
     print('Inicio de sesión exitoso')
     espera
@@ -46,12 +47,11 @@ if 'http://localhost:3000/admin/homeadmin' in driver.current_url:
     driver.get('http://localhost:3000/admin/listusers')  # Reemplaza con la URL real de la ruta a la que deseas redirigir después del inicio de sesión
     espera
 
-    # Configurar Selenium y el navegador (en este caso, utilizaremos Chrome)
-    # Esperar a que el botón de registro esté visible
     register_button = WebDriverWait(driver, 10).until(
         EC.visibility_of_element_located((By.XPATH, "//button[text()='Registrar Usuario']"))
     )
 
+    #    PRUEBA 1 ERROR DE CEDULA Y CORREO
     # Hacer clic en el botón de registro
     register_button.click()
 
@@ -62,30 +62,183 @@ if 'http://localhost:3000/admin/homeadmin' in driver.current_url:
 
     # Rellenar el formulario con los datos necesarios
     first_name_input = register_modal.find_element(By.NAME, "first_name")
-    first_name_input.send_keys("John")
+    first_name_input.send_keys("Maria")
 
     last_name_input = register_modal.find_element(By.NAME, "last_name")
-    last_name_input.send_keys("Doe")
+    last_name_input.send_keys("Mendez")
 
     email_input = register_modal.find_element(By.NAME, "email")
-    email_input.send_keys("john.doe@example.com")
-    espera
-    espera
-    espera
-    # ... Rellenar los demás campos del formulario según sea necesario
+    email_input.send_keys("correo.invalido@gmail.com")
 
-    # Hacer clic en el botón de enviar el formulario
+    phone_number_input = register_modal.find_element(By.NAME, "phone_number")
+    phone_number_input.send_keys("112333")
+
+    identification_type_input = register_modal.find_element(By.NAME, "identification_type")
+    identification_type_input.send_keys("Cédula")
+
+    groups_input = register_modal.find_element(By.NAME, "groups")
+    groups_input.send_keys("Cliente") 
+
+    identification_number_input = register_modal.find_element(By.NAME, "identification_number")
+    identification_number_input.send_keys("1234")
+
+    birth_date_input = register_modal.find_element(By.NAME, "birth_date")
+    birth_date_input.send_keys("25/03/1996")   
+    espera
+    espera  
+    submit_button = register_modal.find_element(By.XPATH, "//button[text()='Crear cuenta']")
+    submit_button.click()   
+    # success_message = WebDriverWait(driver, 10).until(
+    #     EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'Registro Fallido')]"))
+    # )
+    error_button = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//button[text()='Aceptar']"))
+    )
+    # Hacer clic en el botón de registro
+    error_button.click()
+
+    print("Prueba Funcional 1: Errores de Cedula y Correo")
+
+    # PRUEBA 2 ERRORES DE FECHA Y NUMERO DE TELEFONO
+
+    # Hacer clic en el botón de registro
+    register_button.click()   
+
+    # Esperar a que aparezca el formulario de registro modal
+    register_modal = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "exampleModal")) 
+    )
+    # Rellenar el formulario con los datos necesarios
+    first_name_input = register_modal.find_element(By.NAME, "first_name")
+    first_name_input.clear()
+    first_name_input.send_keys("Maria")
+
+    last_name_input = register_modal.find_element(By.NAME, "last_name")
+    last_name_input.clear()
+    last_name_input.send_keys("Mendez")
+
+    email_input = register_modal.find_element(By.NAME, "email")
+    email_input.clear()
+    email_input.send_keys("marimendez@email.com")
+
+    phone_number_input = register_modal.find_element(By.NAME, "phone_number")
+    phone_number_input.clear()
+    phone_number_input.send_keys("3102581")
+
+    identification_type_input = register_modal.find_element(By.NAME, "identification_type")
+    #identification_type_input.clear()
+    identification_type_input.send_keys("Cédula")
+
+    groups_input = register_modal.find_element(By.NAME, "groups")
+    #groups_input.clear()
+    groups_input.send_keys("Cliente") 
+
+    identification_number_input = register_modal.find_element(By.NAME, "identification_number")
+    identification_number_input.clear()
+    identification_number_input.send_keys("74512")
+
+    birth_date_input = register_modal.find_element(By.NAME, "birth_date")
+    #birth_date_input.clear()
+    birth_date_input.send_keys("25/03/2020")
+  
     submit_button = register_modal.find_element(By.XPATH, "//button[text()='Crear cuenta']")
     submit_button.click()
 
     # Esperar a que se muestre el mensaje de éxito o error
-    success_message = WebDriverWait(driver, 10).until(
-        EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'Registro Exitoso')]"))
+    # success_message = WebDriverWait(driver, 10).until(
+    #     EC.visibility_of_element_located((By.XPATH, "//div[contains(text(), 'Registro Exitoso')]"))
+    # )
+
+    espera
+    espera
+    error_button = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//button[text()='Aceptar']"))
     )
 
-    # Obtener el texto del mensaje de éxito o error
-    message_text = success_message.text
-    print(message_text)
+    # Hacer clic en el botón de registro
+    error_button.click()
+
+    print("Prueba Funcional 2: Errores de Telefono y Fecha de Nacimiento")
+
+
+
+   
+
+
+
+    # PRUEBA DE REGISTRO EXITOSA CON TODOS LOS CAMPOS VALIDOS
+    # Hacer clic en el botón de registro
+    register_button.click()   
+
+    # Esperar a que aparezca el formulario de registro modal
+    register_modal = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.ID, "exampleModal")) 
+    )
+    # Rellenar el formulario con los datos necesarios
+    first_name_input = register_modal.find_element(By.NAME, "first_name")
+    first_name_input.clear()
+    first_name_input.send_keys("Maria")
+
+    last_name_input = register_modal.find_element(By.NAME, "last_name")
+    last_name_input.clear()
+    last_name_input.send_keys("Mendez")
+
+    email_input = register_modal.find_element(By.NAME, "email")
+    email_input.clear()
+    email_input.send_keys("marimendez@email.com")
+
+    phone_number_input = register_modal.find_element(By.NAME, "phone_number")
+    phone_number_input.clear()
+    phone_number_input.send_keys("3102581012")
+
+    identification_type_input = register_modal.find_element(By.NAME, "identification_type")
+    #identification_type_input.clear()
+    identification_type_input.send_keys("Cédula")
+
+    groups_input = register_modal.find_element(By.NAME, "groups")
+    #groups_input.clear()
+    groups_input.send_keys("Cliente") 
+
+    identification_number_input = register_modal.find_element(By.NAME, "identification_number")
+    identification_number_input.clear()
+    identification_number_input.send_keys("74512563")
+
+    birth_date_input = register_modal.find_element(By.NAME, "birth_date")
+    #birth_date_input.clear()
+    birth_date_input.send_keys("25/03/1996")
+  
+    submit_button = register_modal.find_element(By.XPATH, "//button[text()='Crear cuenta']")
+    submit_button.click()
+
+    # Esperar a que se muestre el mensaje de éxito o error
+    
+    error_button = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, "//button[text()='Aceptar']"))
+    )
+
+    # Hacer clic en el botón de registro
+    error_button.click()
+
+    espera
+    espera
+
+    print("Prueba Funcional 3: Prueba Funcional con Datos Correctos")
+
+    # Espera a que aparezca el correo electrónico en cualquier celda de la tabla
+    success_message = WebDriverWait(driver, 10).until(
+        EC.visibility_of_element_located((By.XPATH, f"//td[contains(text(), '{email_input.get_attribute('value')}')]"))
+    )
+
+    # wait = WebDriverWait(driver, 10)
+    # wait.until(EC.text_to_be_present_in_element_value((By.XPATH, "//*"), "Texto específico"))
+
+    # # Envía un mensaje cuando se encuentra el texto específico
+    # print("El texto específico ha sido encontrado.")
+    
+
+    # # Obtener el texto del mensaje de éxito o error
+    # message_text = success_message.text
+    # print(message_text)
 
 else:
     print('El inicio de sesión falló')
